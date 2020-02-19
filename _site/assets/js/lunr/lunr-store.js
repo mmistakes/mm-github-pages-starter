8accf83cf49cb73db9b42f8e3be3d0b3a9956def
h4wk590 <aidanbrownj@gmail.com>
2020-02-18 21:26:37 -0800

    Test commit

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "Welcome to The blog!",
+        "excerpt":"Welcome to the Blog! Glad you could stop by. This is the first post of hopefully many! I’m hoping to be active and hold myself to posting here frequently, which I don’t think I’ll have trouble with - lots of things happening in my life that I can blog about....","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "http://localhost:4000/blog/welcome/",
+        "teaser":null},{
+        "title": "My homelab setup with A Raspberry Pi4 and Docker",
+        "excerpt":"This past summer I really got into homelabs, specifically with Raspberry Pis. The Raspberry Pi 4 just came out and it’s specs are better than ever - with up to 4GB or RAM, USB-C charging, and brand new ARM archetecture, makes it a powerful little machine. I wanted to learn...","categories": ["update","tech"],
+        "tags": [],
+        "url": "http://localhost:4000/update/tech/homelab-rpi/",
+        "teaser":null},{
+        "title": "Azure Aks",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/azure-aks/",
+        "teaser":null}]
