9c4178548762937236fbb791d9f0e807b586be64
HBreddam <henrik.breddam@gmail.com>
2020-03-29 16:05:06 +0200

    printversion er nu footer_script

---
+++ assets/js/printversion.js
@@ -0,0 +1,10 @@
+function printversion () {
+    alert("test")
+var prtContent = document.getElementById("opskrift");
+var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+WinPrint.document.write(prtContent.innerHTML);
+WinPrint.document.close();
+WinPrint.focus();
+WinPrint.print();
+WinPrint.close();
+}
