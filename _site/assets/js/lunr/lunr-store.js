eb1bf56f497f8557b1771729a2a0f9b5dd186c61
Christian Henriksen <christian@guzzy.dk>
2022-07-24 15:18:05 +0200

    cleanup

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,19 +1,19 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["blog"],
-        "tags": ["Jekyll","update"],
-        "url": "/blog/welcome-to-jekyll/",
-        "teaser": null
-      },{
         "title": "FDCA - Foreningen for Danske Cyber Alumner",
         "excerpt":"Hvad er FDCA, hvordan bliver man medlem, og hvad laver en cyber forening egentligt? Det vil jeg prøve at beskrive i følgende blog post. This post is in Danish, and will eventually be posted in English Hvad er Cyberværnepligten? Cyberværnepligten er et politisk udspil der skal udklække ~30+ nye digitale...","categories": ["blog"],
         "tags": ["Cyberværnepligt","Cybersecurity","Education"],
-        "url": "/blog/Foreningen-for-danske-cyber-alumner/",
+        "url": "/blog/foreningen-for-danske-cyber-alumner/",
         "teaser": null
       },{
         "title": "Pursuing a Career in Cyberspace",
-        "excerpt":"In this blog post, I will try to enlighten some of the questions that a new candidate in this industry might stumble upon. I will build my points on my own experience being fairly new, therefore might this not apply to you. However, I’m sure that you can relate to...","categories": ["blog"],
+        "excerpt":"In this blog post, I will try to enlighten some of the questions that a new candidate in this industry might stumble upon. I will build my points on my own experience being fairly new, therefore this might not apply to you. However, I’m sure that you can relate to...","categories": ["blog"],
         "tags": ["Career-development","Cybersecurity","Education","Tips & Tricks"],
         "url": "/blog/pursuing-a-career-in-cyber/",
+        "teaser": null
+      },{
+        "title": "WAFW00F - Web Application Firewall Fingerprinting",
+        "excerpt":"In this blog post, I will take a look at a cool tool that I have been working with the last couple of weeks, called WAFW00F. WAFW00F is a tool for fingerprinting if a webserver has a Web Application Firewall. Image is from WAFW00F Github What is a Web Application...","categories": ["blog"],
+        "tags": ["Cybersecurity","Education","Tools","Red teaming","Tips & Tricks"],
+        "url": "/blog/WAFW00F/",
         "teaser": null
       }]
